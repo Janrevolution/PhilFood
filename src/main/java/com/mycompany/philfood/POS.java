870e1ebba7ac58bc6cf42f3a62e4358c0bf960b7
Janrevolution <vjanrevlance@gmail.com>
2023-05-14 23:14:29 +0800

    Fixed POS Table Manipulation

--- src/main/java/com/mycompany/philfood/POS.java
+++ src/main/java/com/mycompany/philfood/POS.java
@@ -383,9 +383,16 @@ public class POS extends javax.swing.JFrame {
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.Integer.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, true, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(jTable1);
